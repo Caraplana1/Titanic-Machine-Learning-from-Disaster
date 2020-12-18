6ee2df41a45b68b6f2300a8af638174ec2ecee0a
Caraplana <cristianlandaez@gmail.com>
2020-12-18 19:48:59 -0400

    Reads all the data from titanic_2

--- RandomForrestRegressor.py
+++ RandomForrestRegressor.py
@@ -7,11 +7,13 @@ titanic_data_train_path = "Data/train.csv"
 titanic_data_test_path = "Data/test.csv"
 
 # Read and drop non aviable data
-titanic_data_train = pd.read_csv(titanic_data_train_path)
+titanic_data_train = pd.read_csv (titanic_data_train_path)
 titanic_data_train.dropna(axis=0)
 
 
-features = ["Pclass", "Sex", "Age"]
+# features to train the model
+features = ["Pclass", "Sex", "Age", "Cabin"]
 
+# Matrixes how keep th data.
 X = titanic_data_train[features]
 y = titanic_data_train.Survived
