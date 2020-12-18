cd82d416d132054b7986f7d456a0b7f69e7c793e
Caraplana <cristianlandaez@gmail.com>
2020-12-18 18:34:14 -0400

    Reads all the data from titanic

--- RandomForrestRegressor.py
+++ RandomForrestRegressor.py
@@ -1,5 +1,17 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
 
+# Datasets paths
+titanic_data_train_path = "Data/train.csv"
+titanic_data_test_path = "Data/test.csv"
+
+# Read and drop non aviable data
+titanic_data_train = pd.read_csv(titanic_data_train_path)
+titanic_data_train.dropna(axis=0)
+
+
+features = ["Pclass", "Sex", "Age"]
+
+X = titanic_data_train[features]
+y = titanic_data_train.Survived
