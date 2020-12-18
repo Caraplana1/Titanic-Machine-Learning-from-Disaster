2dea12f245ffd83ccaf6a11adb05e2d3191f2f5b
Caraplana <cristianlandaez@gmail.com>
2020-12-17 20:11:19 -0400

    Read all the data & put it in variables

--- RandomForrestRegressor.py
+++ RandomForrestRegressor.py
@@ -1,9 +1,17 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
-
-train_data_path = "C:/Users/angel/Desktop/Programación/Python/ML/House Pricing competition/home-data-for-ml-course/train.csv"
-test_data_path = "C:/Users/angel/Desktop/Programación/Python/ML/House Pricing competition/home-data-for-ml-course/test.csv"
+train_data_path = "Data/train.csv"
+test_data_path = "Data/test.csv"
 
 house_data = pd.read_csv(train_data_path)
+test_data = pd.read_csv(test_data_path)
 
 house_data.dropna(axis=0)
+
+features = []
+
+X = house_data[features]
+y = house_data.SalePrice
